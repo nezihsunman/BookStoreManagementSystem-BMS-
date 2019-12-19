df8da5a2b11c779753548662c090c59e74c77b2d
emirgulcur <57862733+emirgulcur@users.noreply.github.com>
2019-12-19 22:28:54 +0300

    Update SearchBookWindow.vue
    Text added

--- frontend/src/components/SearchBookWindow.vue
+++ frontend/src/components/SearchBookWindow.vue
@@ -1,8 +1,17 @@
 <template>
+	<v-card>
+        <v-card-title>
+            Book Search For All Book in Database
+			></v-text-field>
+        </v-card-title>
     <v-data-table>
 
     </v-data-table>
+	 
+
+    </v-card>
 </template>
+
 
 <script>
     export default {
