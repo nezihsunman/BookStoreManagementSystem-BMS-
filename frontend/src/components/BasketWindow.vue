4e51295e0a07f326f2118c6a40cab2452dfab70e
mi014378 <38471029+mi014378@users.noreply.github.com>
2019-12-19 21:31:10 +0300

    Update BasketWindow.vue

--- frontend/src/components/BasketWindow.vue
+++ frontend/src/components/BasketWindow.vue
@@ -1,10 +1,28 @@
 <template>
-    
+    <v-form>
+        <v-container>
+            <v-row>
+                <v-col cols="12" sm="6" md="3">
+                    <v-text-field
+                            label="bookLabel"
+                            v-model="bookName"
+                </v-col>
+                <v-col cols="12" sm="6" md="5">
+                    <v-btn @click="confirmButton()">Confirm Button</v-btn>
+                </v-col>
+            </v-row>
+        </v-container>
+    </v-form>
 </template>
 
 <script>
     export default {
-        name: "BasketWindow"
+        name: "BasketWindow",
+        methods: {
+            confirmButton() {
+                console.log("confirm")
+            }
+        }
     }
 </script>
 
