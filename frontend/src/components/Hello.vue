9956cdc3b326dd49ad7525faa9fa0885bd438e5e
nezihsunman <nezih.sunman@ozu.edu.tr>
2019-12-19 21:42:32 +0300

    hello page of empty component

--- frontend/src/components/Hello.vue
+++ frontend/src/components/Hello.vue
@@ -1,10 +1,24 @@
 <template>
-    
+    <div>
+        <AddBookWindow></AddBookWindow>
+        <BasketWindow></BasketWindow>
+        <LoginWindow></LoginWindow>
+        <OrderBookWindow></OrderBookWindow>
+        <SearchBookWindow></SearchBookWindow>
+    </div>
 </template>
 
 <script>
+    import AddBookWindow from "./AddBookWindow";
+    import BasketWindow from "./BasketWindow";
+    import LoginWindow from "./LoginWindow";
+    import OrderBookWindow from "./OrderBookWindow";
+    import SearchBookWindow from "./SearchBookWindow";
+    import User from "./User";
+
     export default {
-        name: "Hello"
+        name: "Hello",
+        components: {User, SearchBookWindow, OrderBookWindow, LoginWindow, BasketWindow, AddBookWindow},
     }
 </script>
 
