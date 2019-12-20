0865258508e71ba61cd4b2e94967d43ac60b7f49
nezihsunman <nezih.sunman@ozu.edu.tr>
2019-12-20 23:28:01 +0300

    search table added as a view

--- frontend/src/components/SearchBookWindow.vue
+++ frontend/src/components/SearchBookWindow.vue
@@ -1,21 +1,82 @@
 <template>
-	<v-card>
+    <v-card>
         <v-card-title>
             Book Search For All Book in Database
-			></v-text-field>
+            <v-spacer></v-spacer>
+            <v-text-field
+                    v-model="search"
+                    append-icon="search"
+                    label="Search"
+                    single-line
+                    hide-details
+                    item-key="name"
+            ></v-text-field>
         </v-card-title>
-    <v-data-table>
+        <v-data-table class="justify-content-center"
+                      v-model="selected"
+                      :headers="headers"
+                      :items="tableData"
+                      :items-per-page="20"
+                      :search="search"
+                      show-select
+                      :single-select="singleSelect"
+                      item-key="isbn"
 
-    </v-data-table>
-	 
+
+        >
+        </v-data-table>
+        <v-btn @click="addBasket()">Reservation of One Book</v-btn>
+        <v-btn class="justify-content-end" @click="takeDataFromDb()">Take data from db</v-btn>
+
 
     </v-card>
 </template>
 
 
 <script>
+
     export default {
-        name: "SearchBookWindow"
+        name: "SearchBookWindow",
+        data() {
+            return {
+                selected: [],
+                search: '',
+                singleSelect: true,
+                headers: [
+                    {
+                        text: 'Books',
+                        align: 'left',
+                        sortable: true,
+                        value: 'bookName',
+                    },
+                    {text: 'BookType (String)', value: 'bookType'},
+
+                    {text: 'Publish (date)', value: 'publishDate'},
+                    {text: 'ISBN', value: 'isbn'},
+                    {text: 'Version (date)', value: 'versionDate'},
+                    {text: 'BookDate (date)', value: 'BookDate'},
+                    {text: 'NumberOfBook in BookStore', value: 'booknumber'},
+                    {text: 'Price (tl)', value: 'price'},
+                ],
+                tableData: [],
+                dialogShown: false,
+                basketTable: []
+            }
+        },
+        watch: {
+            selected() {
+                if (this.selected[0] != null) {
+                    console.log(this.selected[0].isbn)
+                }
+
+            },
+        },
+        methods: {
+            takeDataFromDb() {
+                this.refreshDataTable()
+            },
+
+        }
     }
 </script>
 
