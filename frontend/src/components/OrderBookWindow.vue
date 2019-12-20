ae017072bd4f2156f6d12e1176fabdfefe7fb582
mi014378 <38471029+mi014378@users.noreply.github.com>
2019-12-21 00:35:29 +0300

    Update OrderBookWindow.vue

--- frontend/src/components/OrderBookWindow.vue
+++ frontend/src/components/OrderBookWindow.vue
@@ -1,13 +1,28 @@
 <template>
-    
+    <v-form>
+        <v-container>
+            <v-row>
+
+                <v-col cols="12" sm="6" md="3">
+                    <v-text-field
+                            label="ISBN"
+                            v-model="bookName"
+                    ></v-text-field>
+                </v-col>
+
+                <v-col cols="12" sm="6" md="3">
+                    <v-text-field
+                            label="Name Of Book"
+                            v-model="isbn"
+                    ></v-text-field>
+                </v-col>
+
+                <v-col cols="12" sm="6" md="5">
+                    <v-btn @click="orderBook()">OrderBook</v-btn>
+                </v-col>
+
+
+            </v-row>
+        </v-container>
+    </v-form>
 </template>
-
-<script>
-    export default {
-        name: "OrderBookWindow"
-    }
-</script>
-
-<style scoped>
-
-</style>
