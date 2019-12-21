705c5bf17efa470e08450332ef23c60944ac3759
nezihsunman <nezih.sunman@ozu.edu.tr>
2019-12-21 17:12:42 +0300

    back end api called change

---
+++ frontend/src/components/backend-api.js
@@ -0,0 +1,47 @@
+import axios from 'axios'
+
+const AXIOS = axios.create({
+    baseURL: `/api`,
+    timeout: 1000
+});
+
+
+export default {
+    hello() {
+        return AXIOS.get(`/hello`);
+    },
+
+    getUser(userId) {
+        return AXIOS.get(`/user/` + userId);
+    },
+    createUser(firstName, lastName) {
+        return AXIOS.post(`/user/` + firstName + '/' + lastName);
+    },
+    getSecured(user, password) {
+        return AXIOS.get(`/secured/`, {
+            auth: {
+                username: user,
+                password: password
+            }
+        });
+    },
+    addbook(firstName, ISBN, bookType, date, publishDate, price, booknumber) {
+        return AXIOS.post('savebook/' + firstName + '/' + ISBN + '/' +
+            bookType + '/' + date + '/' + publishDate + '/' + price + '/' + booknumber
+        )
+    },
+    changeBookNumber(ISBN) {
+        return AXIOS.post('changeBookNumber/' + ISBN)
+    },
+    getTableData() {
+        return axios({
+                method: 'get',
+                url: '/api/takeBookData',
+                responsetype: 'json'
+            }
+        )
+    },
+    getBookByISBN(ISBN) {
+        return AXIOS.post('takeBookByISBN/' + ISBN)
+
+    }
