8c2d5b5959d2f1f3e4064389e3b3fd89a0f709ad
mi014378 <38471029+mi014378@users.noreply.github.com>
2019-12-23 00:43:36 +0300

    Update Hello.vue

--- frontend/src/components/Hello.vue
+++ frontend/src/components/Hello.vue
@@ -1,5 +1,4 @@
-
-	<template>
+<template>
 
     <div v-if="show" class="hello">
         <v-card-text>{{ hellomsg }}</v-card-text>
@@ -50,46 +49,3 @@
     </div>
 
 </template>
-
-<script>
-    import api from "./backend-api";
-
-    import AddBookWindow from "./AddBookWindow";
-    import OrderBookWindow from "./OrderBookWindow";
-    import SearchBookWindow from "./SearchBookWindow";
-
-    export default {
-        name: 'hello',
-        components: {SearchBookWindow, OrderBookWindow, AddBookWindow},
-        props: {hellomsg: {type: String, required: true}},
-        data() {
-            return {
-                show: true,
-                componentShow: false,
-                username: '',
-                loginWindowShow: true,
-                personalName: '',
-                booknumber: '',
-            }
-        },
-        methods: {
-            getUser() {
-                console.log("sss")
-                api.getUserName(this.username).then(response => {
-                    if (response.data != null) {
-                        this.loginWindowShow = false
-                        this.componentShow = true
-                    }
-                }).catch(error => {
-                    console.log(error)
-                })
-
-            }
-        }
-    }
-
-</script>
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
-</style>
