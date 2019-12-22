89887235762c5af85fdc9e1042afd1eb60e95799
nezihsunman <nezih.sunman@ozu.edu.tr>
2019-12-22 13:10:52 +0300

    serach book windows

--- frontend/src/components/SearchBookWindow.vue
+++ frontend/src/components/SearchBookWindow.vue
@@ -25,15 +25,51 @@
 
         >
         </v-data-table>
-        <v-btn @click="addBasket()">Reservation of One Book</v-btn>
+        <v-btn @click="addBasket()">Add Selected To Basket</v-btn>
         <v-btn class="justify-content-end" @click="takeDataFromDb()">Take data from db</v-btn>
 
+        <v-dialog v-model="dialogShown" persistent max-width="900">
+            <template v-slot:activator="{ on }">
+                <v-btn color="primary" dark v-on="on">Basket Window</v-btn>
+            </template>
+            <v-card>
+                <v-card-title class="headline">Basket Of Book</v-card-title>
+                <v-card-text>This is your basket to add to reservation
+                </v-card-text>
+                <v-card-actions>
+                    <v-row>
+                        <v-col cols="12" sm="12" md="12">
+                            <v-data-table class="justify-content-center"
+                                          v-model="selected"
+                                          :headers="headers"
+                                          :items="basketTable"
+                                          :items-per-page="20"
+                                          :search="search"
+                                          item-key="isbn"
+
+
+                            >
+
+                            </v-data-table>
+                        </v-col>
+                        <v-spacer></v-spacer>
+                        <v-col class="align-center" cols="12" sm="12" md="12">
+                            <v-btn color="primary" text @click="closeDialog()">Return</v-btn>
+                            <v-btn color="error" text   @click="complateReservation()">Complate Reservation</v-btn>
+                            <v-btn color="primary" :disabled="true" readonly="true" >Total Price is {{basketTableTotalPrice}}</v-btn>
+                        </v-col>
+                    </v-row>
+
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
 
     </v-card>
+
 </template>
 
-
 <script>
+    import api from "./backend-api";
 
     export default {
         name: "SearchBookWindow",
@@ -60,7 +96,13 @@
                 ],
                 tableData: [],
                 dialogShown: false,
-                basketTable: []
+                basketTable: [],
+                basketTableTotalPrice: 0,
+            }
+        },
+        computed: {
+            refreshPageWhileOpening() {
+                this.takeDataFromDb();
             }
         },
         watch: {
@@ -72,10 +114,49 @@
             },
         },
         methods: {
+            addBasket() {
+                this.basketTable.push(this.selected[0]);
+                this.basketTableTotalPrice=this.basketTableTotalPrice+this.selected[0].price;
+            },
             takeDataFromDb() {
                 this.refreshDataTable()
             },
+            closeDialog() {
+                this.dialogShown = false
+            },
+            deleteOneBook(isbn) {
+                api.changeBookNumber(isbn).then(r => {
+                    console.log("Deleted")
+                }).catch(error => {
+                    console.log("Not deleted")
+                })
 
+            },
+            refreshDataTable() {
+                api.getTableData().then(response => {
+                    this.tableData = response.data
+                }).catch(error => {
+                    console.log(error)
+                })
+                console.log(this.tableData)
+            },
+            takeBookNumberByISBN(ISBN) {
+                api.getBookByISBN(ISBN).then(res => {
+                    this.basketTable.push(JSON.stringify(res.data));
+                    console.log(JSON.flat(this.basketTable));
+                }).catch(err => {
+                    console.log(err);
+                })
+            },
+            complateReservation() {
+                for (var i = 0; i < this.basketTable.length; i++) {
+                    console.log(this.basketTable[i].isbn)
+                    this.deleteOneBook(this.basketTable[i].isbn)
+                }
+                this.dialogShown = false,
+                    this.takeDataFromDb()
+                this.basketTable = []
+            }
         }
     }
 </script>
