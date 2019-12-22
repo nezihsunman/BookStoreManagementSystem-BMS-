6abb2bc5cac05495bfde0e8d5dce71e0723e5663
Enis132435 <56112435+enis132435@users.noreply.github.com>
2019-12-22 19:37:34 +0300

    Order book window of BMS.

--- frontend/src/components/AddBookWindow.vue
+++ frontend/src/components/AddBookWindow.vue
@@ -10,6 +10,7 @@
                     ></v-text-field>
                 </v-col>
 
+
                 <v-col cols="12" sm="6" md="3">
                     <v-text-field
                             label="ISBN"
