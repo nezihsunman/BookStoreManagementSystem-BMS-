7c61aac2eeeb44b47f9d4dc351159155b6391ef9
emirgulcur <57862733+emirgulcur@users.noreply.github.com>
2019-12-22 22:06:02 +0300

    Method to get user

--- frontend/src/components/Hello.vue
+++ frontend/src/components/Hello.vue
@@ -29,10 +29,24 @@
                 booknumber: '',
             }
         },
-		
-    }
-</script>
+      methods: {
+            getUser() {
+                console.log("sss")
+                api.getUserName(this.username).then(response => {
+                    if (response.data != null) {
+                        this.loginWindowShow = false
+                        this.componentShow = true
+                    }
+                }).catch(error => {
+                    console.log(error)
+                })
 
+            }
+        }
+    }
+
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 
 </style>
