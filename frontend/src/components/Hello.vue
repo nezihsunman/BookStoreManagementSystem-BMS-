6a63a99b4a889fcd210c1aa58d34bdb2e5076ff5
mi014378 <38471029+mi014378@users.noreply.github.com>
2019-12-23 12:40:28 +0300

    Update Hello.vue

--- frontend/src/components/Hello.vue
+++ frontend/src/components/Hello.vue
@@ -49,3 +49,48 @@
     </div>
 
 </template>
+
+<script>
+    import api from "./backend-api";
+
+    import AddBookWindow from "./AddBookWindow";
+    import BasketWindow from "./BasketWindow";
+    import OrderBookWindow from "./OrderBookWindow";
+    import SearchBookWindow from "./SearchBookWindow";
+    import User from "./User";
+
+    export default {
+        name: 'hello',
+        components: {User, SearchBookWindow, OrderBookWindow, BasketWindow, AddBookWindow},
+        props: {hellomsg: {type: String, required: true}},
+        data() {
+            return {
+                show: true,
+                componentShow: false,
+                username: '',
+                loginWindowShow: true,
+                personalName: '',
+                booknumber: '',
+            }
+        },
+        methods: {
+            getUser() {
+                console.log("sss")
+                api.getUserName(this.username).then(response => {
+                    if (response.data != null) {
+                        this.loginWindowShow = false
+                        this.componentShow = true
+                    }
+                }).catch(error => {
+                    console.log(error)
+                })
+
+            }
+        }
+    }
+
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
